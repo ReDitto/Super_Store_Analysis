cb4a34f36ef3707f69a09095d9ed2eaaf9fd9af0
ReDitto <98224753+ReDitto@users.noreply.github.com>
2021-09-07 07:36:02 +0000

    0.2.25

--- src/index.js
+++ src/index.js
@@ -4,4 +4,28 @@ import './index.css';
 import Analytics from './Analytics';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-impor
+import 'typeface-roboto';
+
+window.addEventListener('beforeinstallprompt', async (event) => {
+  const choiceResult = await event.userChoice();
+  Analytics.logEvent('app', 'prompt', choiceResult.outcome);
+});
+
+window.addEventListener('appinstalled', (event) => {
+  Analytics.logEvent('app', 'install', undefined, 1);
+});
+
+let resolveUpdateAvailable;
+let resolveCacheComplete;
+window.updateAvailable = new Promise((resolve) => {
+  resolveUpdateAvailable = resolve;
+});
+window.cacheComplete = new Promise((resolve) => {
+  resolveCacheComplete = resolve;
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service
