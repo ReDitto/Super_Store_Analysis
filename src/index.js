c4fa504ca9bbe2b1a9236fd27c9c78b108db37b4
ReDitto <98224753+ReDitto@users.noreply.github.com>
2021-09-07 11:56:32 +0000

    ring-buffer-benchmark: Fix the wrong sched_priority of producer

--- src/index.js
+++ src/index.js
@@ -28,4 +28,12 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register({
+  onUpdate: (registration) => {
+    resolveUpdateAvailable(registration);
+  },
+  onSuccess: (registration) => {
+    resolveCacheComplete(registration);
+  },
+});
